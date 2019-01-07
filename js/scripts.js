56dbca5b1f8c81c4e034bd59f1f187d1177fc218
indranil2501 <indranil2501@gmail.com>
2019-01-07 16:52:32 +0530

    Footer Done

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#btnloginModal").click(function(){
         $("#loginModal").modal("show");
     });
+
+    $("#goToTop").click(function(){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
 });
